16457502f77c7d77d779ab31f0e0f000c10e7ab0
LanceXXXX <mastergarox@hotmail.com>
2016-10-16 14:05:34 -0400

    Add files via upload

---
+++ customizable_loadouts/lua/autorun/Client/cl_client.lua
@@ -0,0 +1,100 @@
+function loadout_spawn()
+local ply = LocalPlayer()
+
+local cl_hl2 = { "weapon_crowbar", "weapon_pistol", "weapon_357", "weapon_smg1", "weapon_ar2", "weapon_shotgun", "weapon_crossbow", "weapon_frag", "weapon_rpg", "weapon_physcannon", "weapon_bugbait" }
+local cl_hl2_names = { "Crowbar", "9mm Pistol", ".357 Magnum", "SMG", "Pulse Rifle", "Shotgun", "Crossbow", "Frag Grenade", "RPG", "Gravity Gun", "Bug Bait" }
+cl_weapons = { }
+
+for i = 1, 11 do
+table.insert(cl_weapons,{ Spawnable = true, AdminOnly = false, ClassName = cl_hl2[i], PrintName = cl_hl2_names[i], Category = "Half-Life 2" } )
+end
+
+local cl_hl2dm = { "weapon_stunstick", "weapon_slam" }
+local cl_hl2dm_names = { "Stun Stick", "S.L.A.M" }
+
+for i = 1, 2 do
+table.insert(cl_weapons,{ Spawnable = true, AdminOnly = false, ClassName = cl_hl2dm[i], PrintName = cl_hl2dm_names[i], Category = "Half-Life 2: Deathmatch" } )
+end
+
+local cl_gmod = { "weapon_physgun" }
+local cl_gmod_names = { "Physics Gun" }
+
+table.insert(cl_weapons,{ Spawnable = true, AdminOnly = false, ClassName = cl_gmod[1], PrintName = cl_gmod_names[1], Category = "Other" } )
+
+table.Add(cl_weapons,weapons.GetList())
+
+cl_loadout = { }
+
+for k, v in pairs(cl_weapons) do
+if ply:HasWeapon(v.ClassName) then
+cl_loadout[v.ClassName] = true
+else
+cl_loadout[v.ClassName] = false
+end
+end
+net.Start("loadout_c2s")
+net.SendToServer()
+end
+
+function loadout_option()
+spawnmenu.AddToolMenuOption("Utilities","User","Loadout Option","Customize Loadout","","", loadout_panel,{})
+end
+
+function loadout_panel(panel)
+panel:ClearControls()
+
+local cl_clabel = { }
+local cl_cnum = 0
+
+local cl_ci = { }
+
+local cl_skip = false
+
+for k, v in pairs(cl_weapons) do
+if v.Spawnable != nil && v.ClassName != nil && v.PrintName != nil then
+if v.Category == nil then v.Category = "Other" end
+cl_skip = false
+if v.AdminOnly != nil then if v.AdminOnly then cl_skip = true end end
+if v.Spawnable && !cl_skip then
+
+cl_clabel[cl_cnum] = vgui.Create("DCollapsibleCategory",panel)
+if cl_clabel[v.Category] != nil then
+cl_clabel[cl_cnum]:Remove()
+else
+cl_clabel[cl_cnum]:SetLabel(v.Category)
+cl_clabel[cl_cnum]:SetExpanded(1)
+cl_clabel[cl_cnum]:Dock(TOP)
+
+cl_clabel[v.Category] = cl_clabel[cl_cnum]
+cl_ci[v.Category] = 1
+cl_cnum = cl_cnum + 1
+end
+
+v.cl_label = vgui.Create("DCheckBoxLabel",cl_clabel[v.Category])
+v.cl_label:SetTextColor(Color(0,0,0))
+v.cl_label:SetText(v.PrintName)
+v.cl_label:SetPos(25,25 * cl_ci[v.Category])
+if cl_loadout[v.ClassName] then
+v.cl_label:SetValue(true)
+else
+v.cl_label:SetValue(false)
+end
+function v.cl_label:OnChange(checked)
+if checked then
+net.Start("loadout_giveweapon")
+net.WriteInt(k,16)
+net.SendToServer()
+else
+net.Start("loadout_removeweapon")
+net.WriteInt(k,16)
+net.SendToServer()
+end
+end
+cl_ci[v.Category] = cl_ci[v.Category] + 1
+end
+end
+end
+end
+
+hook.Add("InitPostEntity","Loadout Spawn",loadout_spawn)
+hook.Add("PopulateToolMenu","Loadout Option Hook",loadout_option)
---
+++ customizable_loadouts/lua/autorun/Server/cl_server.lua
@@ -0,0 +1,156 @@
+net.Receive("loadout_c2s",function(len,ply)
+ply.cl_spawned = true
+
+ply.cl_loadout = { }
+
+ply:RemoveAllAmmo()
+for k, v in pairs(cl_weapons) do
+if ply:HasWeapon(v.ClassName) then
+ply.cl_loadout[v.ClassName] = true
+else
+ply.cl_loadout[v.ClassName] = false
+end
+if v.Primary.Ammo != nil then
+if v.Primary.Ammo != "none" then
+if v.Primary.ClipSize != nil then
+if v.Primary.ClipSize > 0 then
+ply:GiveAmmo(v.Primary.ClipSize * 16,v.Primary.Ammo,true)
+else
+ply:GiveAmmo(16,v.Primary.Ammo,true)
+end
+else
+ply:GiveAmmo(16,v.Primary.Ammo,true)
+end
+end
+end
+if v.Secondary.Ammo != nil then
+if v.Secondary.Ammo != "none" then
+if v.Secondary.ClipSize != nil then
+if v.Secondary.ClipSize > 0 then
+ply:GiveAmmo(v.Secondary.ClipSize * 16,v.Secondary.Ammo,true)
+else
+ply:GiveAmmo(16,v.Secondary.Ammo,true)
+end
+else
+ply:GiveAmmo(16,v.Secondary.Ammo,true)
+end
+end
+end
+end
+end)
+
+net.Receive("loadout_giveweapon",function(len,ply)
+local cl_weapon = net.ReadInt(16)
+ply.cl_loadout[cl_weapons[cl_weapon].ClassName] = true
+ply:Give(cl_weapons[cl_weapon].ClassName)
+if cl_weapons[cl_weapon].Primary.Ammo != nil then
+if cl_weapons[cl_weapon].Primary.Ammo != "none" then
+if cl_weapons[cl_weapon].Primary.ClipSize != nil then
+if cl_weapons[cl_weapon].Primary.ClipSize > 0 then
+ply:GiveAmmo(cl_weapons[cl_weapon].Primary.ClipSize * 16,cl_weapons[cl_weapon].Primary.Ammo,true)
+else
+ply:GiveAmmo(16,cl_weapons[cl_weapon].Primary.Ammo,true)
+end
+else
+ply:GiveAmmo(16,cl_weapons[cl_weapon].Primary.Ammo,true)
+end
+end
+end
+if cl_weapons[cl_weapon].Secondary.Ammo != nil then
+if cl_weapons[cl_weapon].Secondary.Ammo != "none" then
+if cl_weapons[cl_weapon].Secondary.ClipSize != nil then
+if cl_weapons[cl_weapon].Secondary.ClipSize > 0 then
+ply:GiveAmmo(cl_weapons[cl_weapon].Secondary.ClipSize * 16,cl_weapons[cl_weapon].Secondary.Ammo,true)
+else
+ply:GiveAmmo(16,cl_weapons[cl_weapon].Secondary.Ammo,true)
+end
+else
+ply:GiveAmmo(16,cl_weapons[cl_weapon].Secondary.Ammo,true)
+end
+end
+end
+end)
+
+net.Receive("loadout_removeweapon",function(len,ply)
+local cl_weapon = net.ReadInt(16)
+ply.cl_loadout[cl_weapons[cl_weapon].ClassName] = false
+ply:StripWeapon(cl_weapons[cl_weapon].ClassName)
+end)
+
+function loadout_start()
+local cl_hl2 = { "weapon_crowbar", "weapon_pistol", "weapon_357", "weapon_smg1", "weapon_ar2", "weapon_shotgun", "weapon_crossbow", "weapon_frag", "weapon_rpg", "weapon_physcannon", "weapon_bugbait" }
+local cl_hl2_names = { "Crowbar", "9mm Pistol", ".357 Magnum", "SMG", "Pulse Rifle", "Shotgun", "Crossbow", "Frag Grenade", "RPG", "Gravity Gun", "Bug Bait" }
+
+local cl_hl2_ammo = { "none", "Pistol", "357", "SMG1", "AR2", "Buckshot", "XBowBolt", "Grenade", "RPG_Round", "none", "none" }
+local cl_hl2_clip = { -1, 18, 6, 45, 30, 6, 1, -1, -1, -1, -1 }
+
+local cl_hl2_ammo2 = { "none", "none", "none", "SMG1_Grenade", "AR2AltFire", "none", "none", "Grenade", "RPG_Round", "none", "none" }
+local cl_hl2_clip2 = { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 }
+
+cl_weapons = { }
+
+for i = 1, 11 do
+table.insert(cl_weapons,{ Spawnable = true, AdminOnly = false, ClassName = cl_hl2[i], PrintName = cl_hl2_names[i], Category = "Half-Life 2", Primary = { Ammo = cl_hl2_ammo[i], ClipSize = cl_hl2_clip[i] }, Secondary = { Ammo = cl_hl2_ammo2[i], ClipSize = cl_hl2_clip2[i] } } )
+end
+
+local cl_hl2dm = { "weapon_stunstick", "weapon_slam" }
+local cl_hl2dm_names = { "Stun Stick", "S.L.A.M" }
+
+local cl_hl2dm_ammo = { "none", "slam" }
+local cl_hl2dm_clip = { -1, -1 }
+
+local cl_hl2dm_ammo2 = { "none", "none" }
+local cl_hl2dm_clip2 = { -1, -1 }
+
+for i = 1, 2 do
+table.insert(cl_weapons,{ Spawnable = true, AdminOnly = false, ClassName = cl_hl2dm[i], PrintName = cl_hl2dm_names[i], Category = "Half-Life 2: Deathmatch", Primary = { Ammo = cl_hl2dm_ammo[i], ClipSize = cl_hl2dm_clip[i] }, Secondary = { Ammo = cl_hl2dm_ammo2[i], ClipSize = cl_hl2dm_clip2[i] } } )
+end
+
+local cl_gmod = { "weapon_physgun" }
+local cl_gmod_names = { "Physics Gun" }
+
+table.insert(cl_weapons,{ Spawnable = true, AdminOnly = false, ClassName = cl_gmod[1], PrintName = cl_gmod_names[1], Category = "Other", Primary = { }, Secondary = { } } )
+
+table.Add(cl_weapons,weapons.GetList())
+end
+
+function loadout_respawn(ply)
+if ply.cl_spawned == nil then return end
+
+ply:RemoveAllAmmo()
+for k, v in pairs(cl_weapons) do
+if ply.cl_loadout[v.ClassName] then
+ply:Give(v.ClassName)
+if v.Primary.Ammo != nil then
+if v.Primary.Ammo != "none" then
+if v.Primary.ClipSize != nil then
+if v.Primary.ClipSize > 0 then
+ply:GiveAmmo(v.Primary.ClipSize * 16,v.Primary.Ammo,true)
+else
+ply:GiveAmmo(16,v.Primary.Ammo,true)
+end
+end
+end
+end
+if v.Secondary.Ammo != nil then
+if v.Secondary.Ammo != "none" then
+if v.Secondary.ClipSize != nil then
+if v.Secondary.ClipSize > 0 then
+ply:GiveAmmo(v.Secondary.ClipSize * 16,v.Secondary.Ammo,true)
+else
+ply:GiveAmmo(16,v.Secondary.Ammo,true)
+end
+end
+end
+end
+end
+end
+return true
+end
+
+hook.Add("Initialize","Loadout Start",loadout_start)
+hook.Add("PlayerLoadout","Loadout Respawn",loadout_respawn)
+
+util.AddNetworkString("loadout_c2s")
+util.AddNetworkString("loadout_giveweapon")
+util.AddNetworkString("loadout_removeweapon")
