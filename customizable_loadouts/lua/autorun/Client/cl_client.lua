f706564401729452fc8e6af53dba3f2edc8e07e4
LanceXXXX <mastergarox@hotmail.com>
2016-10-27 18:17:13 -0400

    Update cl_client.lua

--- customizable_loadouts/lua/autorun/Client/cl_client.lua
+++ customizable_loadouts/lua/autorun/Client/cl_client.lua
@@ -1,4 +1,4 @@
-function loadout_spawn()
+function loadout_initialize()
 local ply = LocalPlayer()
 
 local cl_hl2 = { "weapon_crowbar", "weapon_pistol", "weapon_357", "weapon_smg1", "weapon_ar2", "weapon_shotgun", "weapon_crossbow", "weapon_frag", "weapon_rpg", "weapon_physcannon", "weapon_bugbait" }
@@ -25,14 +25,23 @@ table.Add(cl_weapons,weapons.GetList())
 
 cl_loadout = { }
 
+local cl_readweapons = file.Read("cl_weapons.txt","DATA")
+local cl_storedweapons = { }
+if cl_readweapons != nil then
+cl_storedweapons = string.Explode("|",cl_readweapons,true)
+for k, v in pairs(cl_storedweapons) do
+cl_loadout[v] = true
+end
+else
 for k, v in pairs(cl_weapons) do
 if ply:HasWeapon(v.ClassName) then
 cl_loadout[v.ClassName] = true
-else
-cl_loadout[v.ClassName] = false
+file.Append("cl_weapons.txt","|" .. v.ClassName)
+end
 end
 end
 net.Start("loadout_c2s")
+net.WriteTable(cl_storedweapons)
 net.SendToServer()
 end
 
@@ -74,17 +83,31 @@ v.cl_label = vgui.Create("DCheckBoxLabel",cl_clabel[v.Category])
 v.cl_label:SetTextColor(Color(0,0,0))
 v.cl_label:SetText(v.PrintName)
 v.cl_label:SetPos(25,25 * cl_ci[v.Category])
-if cl_loadout[v.ClassName] then
+if cl_loadout[v.ClassName] != nil then
 v.cl_label:SetValue(true)
 else
 v.cl_label:SetValue(false)
 end
 function v.cl_label:OnChange(checked)
 if checked then
+local cl_readweapons = string.Explode("|",file.Read("cl_weapons.txt","DATA"),true)
+
+for k2, v2 in pairs(cl_readweapons) do
+if v2 == v.ClassName then return end
+end
+
+file.Append("cl_weapons.txt","|" .. v.ClassName)
+
 net.Start("loadout_giveweapon")
 net.WriteInt(k,16)
 net.SendToServer()
 else
+local cl_readweapons = string.Explode("|",file.Read("cl_weapons.txt","DATA"),true)
+
+for k2, v2 in pairs(cl_readweapons) do
+if v2 == v.ClassName then table.remove(cl_readweapons,k2) file.Write("cl_weapons.txt",string.Implode("|",cl_readweapons)) break end
+end
+
 net.Start("loadout_removeweapon")
 net.WriteInt(k,16)
 net.SendToServer()
@@ -104,15 +127,24 @@ cl_unmarkall:SetTextColor(Color(0,0,0))
 cl_unmarkall:SetText("Unmark All")
 cl_unmarkall:SetPos(25,25)
 function cl_unmarkall:DoClick()
+local cl_readweapons = string.Explode("|",file.Read("cl_weapons.txt","DATA"),true)
+
 for k, v in pairs(cl_weapons) do
-if cl_loadout[v.ClassName] then
+for k2, v2 in pairs(cl_readweapons) do
+if v2 == v.ClassName then
 v.cl_label:SetValue(false)
+cl_loadout[v.ClassName] = nil
+table.remove(cl_readweapons,k2)
 end
 end
+end
+
+file.Write("cl_weapons.txt",string.Implode("|",cl_readweapons))
+
 net.Start("loadout_unmarkall")
 net.SendToServer()
 end
 end
 
-hook.Add("InitPostEntity","Loadout Spawn",loadout_spawn)
+hook.Add("InitPostEntity","Loadout Initialize",loadout_initialize)
 hook.Add("PopulateToolMenu","Loadout Option Hook",loadout_option)
