28d5ee716dd8ced0b38b15f5cb388ef1c35c9aca
LanceXXXX <mastergarox@hotmail.com>
2016-10-27 18:18:05 -0400

    Update cs_client.lua

--- client_sounds/lua/autorun/Client/cs_client.lua
+++ client_sounds/lua/autorun/Client/cs_client.lua
@@ -1,26 +1,34 @@
-function clientsound_c2s(ply,cmd,args)
+function clientsounds_client(ply,cmd,args)
 if args[1] == nil then return end
-net.Start("clientsound_server")
+net.Start("clientsounds_clientc2s")
 net.WriteString(args[1])
 
 net.SendToServer()
 end
 
-function globalsound_c2s(ply,cmd,args)
+function clientsounds_global(ply,cmd,args)
 if args[1] == nil then return end
-net.Start("globalsound_server")
+net.Start("clientsounds_globalc2s")
 net.WriteString(args[1])
 
 net.SendToServer()
 end
 
-function globalstopsound_c2s(ply,cmd,args)
-net.Start("globalstopsound_server")
+function clientsounds_globalstop(ply,cmd,args)
+net.Start("clientsounds_globalstopc2s")
 
 net.SendToServer()
 end
 
-function localsound_client(ply,cmd,args)
+function clientsounds_stream(ply,cmd,args)
+if args[1] == nil then return end
+net.Start("clientsounds_streamc2s")
+net.WriteString(args[1])
+
+net.SendToServer()
+end
+
+function clientsounds_local(ply,cmd,args)
 if args[1] == nil then return end
 local cs_sound = args[1]
 
@@ -35,23 +43,94 @@ end
 RunConsoleCommand("play",cs_sound .. cs_extension)
 end
 
-function stopsound_client(ply,cmd,args)
+function clientsounds_stop(ply,cmd,args)
 RunConsoleCommand("stopsound")
 end
 
-net.Receive("globalsound_s2c",function(len,ply)
+net.Receive("clientsounds_globals2c",function(len,ply)
 local cs_sound = net.ReadString()
 
 RunConsoleCommand("play",cs_sound)
 end)
 
-net.Receive("globalstopsound_s2c",function(len,ply)
+net.Receive("clientsounds_globalstops2c",function(len,ply)
 RunConsoleCommand("stopsound")
 end)
 
-concommand.Add("snd",clientsound_c2s)
-concommand.Add("gsnd",globalsound_c2s)
-concommand.Add("gssnd",globalstopsound_c2s)
+net.Receive("clientsounds_streams2c",function(len,ply)
+if cs_blockurl > 0 then return end
+local cs_url = net.ReadString()
 
-concommand.Add("lsnd",localsound_client)
-concommand.Add("ssnd",stopsound_client)
+sound.PlayURL(cs_url,"noplay",function(url)
+
+if IsValid(url) then
+if cs_currenturl != nil then cs_currenturl:Stop() end
+cs_currenturl = url
+url:SetVolume(cs_volume)
+url:Play()
+else
+LocalPlayer():ChatPrint("Invalid URL! Try accessing the stream directly.")
+end
+end)
+end)
+
+function clientsounds_cvar_volume(ply,cmd,args)
+local cs_cvar_volume = args[1]
+if cs_cvar_volume == nil then
+print("cs_client_urlvolume" .. " = " .. cs_volume .. " ( def. \"1\" )\n - Controls the playback volume of streams.")
+else
+cs_cvar_volume = tonumber(cs_cvar_volume)
+if cs_cvar_volume == nil then return end
+if cs_cvar_volume > 1 then cs_volume = 1 elseif cs_cvar_volume < 0 then cs_volume = 0 else cs_volume = cs_cvar_volume end
+if cs_currenturl != nil then cs_currenturl:SetVolume(cs_volume) end
+
+file.Write("cs_volume.txt",cs_volume)
+end
+end
+
+function clientsounds_cvar_blockurl(ply,cmd,args)
+local cs_cvar_blockurl = args[1]
+if cs_cvar_blockurl == nil then
+print("cs_client_blockurl" .. " = " .. cs_blockurl .. " ( def. \"0\" )\n - Blocks the playback of streams.")
+else
+cs_cvar_blockurl = tonumber(cs_cvar_blockurl)
+if cs_cvar_blockurl == nil then return end
+if cs_cvar_blockurl > 1 then cs_blockurl = 1 elseif cs_cvar_blockurl < 0 then cs_blockurl = 0 else cs_blockurl = cs_cvar_blockurl end
+if cs_currenturl != nil then if cs_blockurl > 0 then cs_currenturl:Stop() end end
+
+file.Write("cs_blockurl.txt",cs_blockurl)
+end
+end
+
+function clientsounds_initialize()
+local cs_cvar_volume = file.Read("cs_volume.txt","DATA")
+if cs_cvar_volume != nil then
+cs_cvar_volume = tonumber(cs_cvar_volume)
+if cs_cvar_volume == nil then return end
+cs_volume = cs_cvar_volume
+else
+cs_volume = 1
+end
+
+local cs_cvar_blockurl = file.Read("cs_blockurl.txt","DATA")
+if cs_cvar_blockurl != nil then
+cs_cvar_blockurl = tonumber(cs_cvar_blockurl)
+if cs_cvar_blockurl == nil then return end
+cs_blockurl = cs_cvar_blockurl
+else
+cs_blockurl = 0
+end
+end
+
+concommand.Add("snd",clientsounds_client)
+concommand.Add("gsnd",clientsounds_global)
+concommand.Add("gssnd",clientsounds_globalstop)
+concommand.Add("url",clientsounds_stream)
+
+concommand.Add("lsnd",clientsounds_local)
+concommand.Add("ssnd",clientsounds_stop)
+
+concommand.Add("cs_client_urlvolume",clientsounds_cvar_volume)
+concommand.Add("cs_client_blockurl",clientsounds_cvar_blockurl)
+
+hook.Add("Initialize","Clientsounds Initialize",clientsounds_initialize)
