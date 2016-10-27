67bcea9a93c792cf04597126e8dc0cb6226fd455
LanceXXXX <mastergarox@hotmail.com>
2016-10-27 18:18:23 -0400

    Update cs_server.lua

--- client_sounds/lua/autorun/Server/cs_server.lua
+++ client_sounds/lua/autorun/Server/cs_server.lua
@@ -1,4 +1,4 @@
-net.Receive("clientsound_server",function(len,ply)
+net.Receive("clientsounds_clientc2s",function(len,ply)
 local cs_sound = net.ReadString()
 
 local cs_extension = string.GetExtensionFromFilename("garrysmod/sound/" .. cs_sound)
@@ -9,10 +9,10 @@ else
 cs_sound = string.sub(cs_sound,1,string.len(cs_sound) - string.len(cs_extension))
 end
 
-ply:EmitSound(cs_sound .. cs_extension,cvars.Number("cs_dropoff",86),100,1,CHAN_REPLACE)
+ply:EmitSound(cs_sound .. cs_extension,cs_dropoff,100,1,CHAN_REPLACE)
 end)
 
-net.Receive("globalsound_server",function(len,ply)
+net.Receive("clientsounds_globalc2s",function(len,ply)
 
 if !ply:IsAdmin() && !ply:IsSuperAdmin() then
 ply:ChatPrint("This function is restricted to admins!")
@@ -30,13 +30,13 @@ cs_sound = string.sub(cs_sound,1,string.len(cs_sound) - string.len(cs_extension)
 end
 
 PrintMessage(3,ply:GetName() .. " played " .. cs_sound .. cs_extension)
-net.Start("globalsound_s2c")
+net.Start("clientsounds_globals2c")
 net.WriteString(cs_sound .. cs_extension)
 
 net.Broadcast()
 end)
 
-net.Receive("globalstopsound_server",function(len,ply)
+net.Receive("clientsounds_globalstopc2s",function(len,ply)
 
 if !ply:IsAdmin() && !ply:IsSuperAdmin() then
 ply:ChatPrint("This function is restricted to admins!")
@@ -44,12 +44,28 @@ return
 end
 
 PrintMessage(3,ply:GetName() .. " stopped all sounds!")
-net.Start("globalstopsound_s2c")
+net.Start("clientsounds_globalstops2c")
 
 net.Broadcast()
 end)
 
-function text_sound(ply,text,team)
+net.Receive("clientsounds_streamc2s",function(len,ply)
+
+if !ply:IsAdmin() && !ply:IsSuperAdmin() then
+ply:ChatPrint("This function is restricted to admins!")
+return
+end
+
+local cs_url = net.ReadString()
+
+PrintMessage(3,ply:GetName() .. " opened " .. cs_url)
+net.Start("clientsounds_streams2c")
+net.WriteString(cs_url)
+
+net.Broadcast()
+end)
+
+function clientsounds_textsound(ply,text,team)
 if string.sub(text,1,1) == "." then
 cs_global = false
 elseif string.sub(text,1,1) == "," then
@@ -76,7 +92,7 @@ else
 cs_sound = string.sub(cs_sound,1,string.len(cs_sound) - string.len(cs_extension))
 end
 
-ply:EmitSound(cs_sound .. cs_extension,cvars.Number("cs_dropoff",86),100,1,CHAN_REPLACE)
+ply:EmitSound(cs_sound .. cs_extension,cs_dropoff,100,1,CHAN_REPLACE)
 elseif ply:IsAdmin() || ply:IsSuperAdmin() then
 
 local cs_sound = string.sub(text,2)
@@ -91,7 +107,7 @@ cs_sound = string.sub(cs_sound,1,string.len(cs_sound) - string.len(cs_extension)
 end
 
 PrintMessage(3,ply:GetName() .. " played " .. cs_sound .. cs_extension)
-net.Start("globalsound_s2c")
+net.Start("clientsounds_globals2c")
 net.WriteString(cs_sound .. cs_extension)
 
 net.Broadcast()
@@ -100,13 +116,40 @@ ply:ChatPrint("This function is restricted to admins!")
 end
 end
 
-hook.Add("PlayerSay","Text Sound",text_sound)
+function clientsounds_cvar_dropoff(ply,cmd,args)
+local cs_cvar_dropoff = args[1]
+if cs_cvar_dropoff == nil then
+print("cs_dropoff" .. " = " .. cs_dropoff .. " ( def. \"86\" )\n - Alters the dropoff of client-played sounds. Valid range is from 0 to 511.")
+else
+cs_cvar_dropoff = tonumber(cs_cvar_dropoff)
+if cs_cvar_dropoff == nil then return end
+if cs_cvar_dropoff > 511 then cs_dropoff = 511 elseif cs_cvar_dropoff < 0 then cs_dropoff = 0 else cs_dropoff = cs_cvar_dropoff end
 
-util.AddNetworkString("clientsound_server")
-util.AddNetworkString("globalsound_server")
-util.AddNetworkString("globalstopsound_server")
+file.Write("cs_dropoff.txt",cs_dropoff)
+end
+end
 
-util.AddNetworkString("globalsound_s2c")
-util.AddNetworkString("globalstopsound_s2c")
+function clientsounds_initialize()
+local cs_cvar_dropoff = file.Read("cs_dropoff.txt","DATA")
+if cs_cvar_dropoff != nil then
+cs_cvar_dropoff = tonumber(cs_cvar_dropoff)
+if cs_cvar_dropoff == nil then return end
+cs_dropoff = cs_cvar_dropoff
+else
+cs_dropoff = 86
+end
+end
 
-CreateConVar("cs_dropoff",86,FCVAR_ARCHIVE,"Alters the dropoff of client-played sounds. Valid range is from 0 to 511. Default is 86.")
+util.AddNetworkString("clientsounds_clientc2s")
+util.AddNetworkString("clientsounds_globalc2s")
+util.AddNetworkString("clientsounds_globalstopc2s")
+util.AddNetworkString("clientsounds_streamc2s")
+
+util.AddNetworkString("clientsounds_globals2c")
+util.AddNetworkString("clientsounds_globalstops2c")
+util.AddNetworkString("clientsounds_streams2c")
+
+hook.Add("PlayerSay","Clientsounds Text Sound",clientsounds_textsound)
+hook.Add("Initialize","Clientsounds Initialize",clientsounds_initialize)
+
+concommand.Add("cs_dropoff",clientsounds_cvar_dropoff)
