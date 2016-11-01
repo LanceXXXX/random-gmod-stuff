b43122bb700b8f69f6422cba8c79d0d941155ddd
LanceXXXX <mastergarox@hotmail.com>
2016-11-01 05:00:15 -0400

    Update cs_server.lua

--- client_sounds/lua/autorun/Server/cs_server.lua
+++ client_sounds/lua/autorun/Server/cs_server.lua
@@ -1,6 +1,5 @@
 net.Receive("clientsounds_client_c2s",function(len,ply)
 local cs_sound = net.ReadString()
-
 local cs_extension = string.GetExtensionFromFilename("garrysmod/sound/" .. cs_sound)
 
 if cs_extension == nil then
@@ -15,6 +14,13 @@ net.WriteString(cs_sound .. cs_extension)
 net.Broadcast()
 end)
 
+net.Receive("clientsounds_random_c2s",function(len,ply)
+net.Start("clientsounds_random_s2c")
+net.WriteEntity(ply)
+net.WriteString(cs_random[math.random(1,cs_max)])
+net.Broadcast()
+end)
+
 net.Receive("clientsounds_global_c2s",function(len,ply)
 
 if !ply:IsAdmin() && !ply:IsSuperAdmin() then
@@ -23,7 +29,6 @@ return
 end
 
 local cs_sound = net.ReadString()
-
 local cs_extension = string.GetExtensionFromFilename("garrysmod/sound/" .. cs_sound)
 
 if cs_extension == nil then
@@ -124,14 +129,22 @@ ply:ChatPrint("This function is restricted to admins!")
 end
 end
 
+function clientsounds_initialize()
+cs_random = file.Find("sound/*.ogg","GAME","namedesc")
+cs_max = table.getn(cs_random)
+end
+
 util.AddNetworkString("clientsounds_client_c2s")
+util.AddNetworkString("clientsounds_random_c2s")
 util.AddNetworkString("clientsounds_global_c2s")
 util.AddNetworkString("clientsounds_globalstop_c2s")
 util.AddNetworkString("clientsounds_stream_c2s")
 
 util.AddNetworkString("clientsounds_client_s2c")
+util.AddNetworkString("clientsounds_random_s2c")
 util.AddNetworkString("clientsounds_global_s2c")
 util.AddNetworkString("clientsounds_globalstop_s2c")
 util.AddNetworkString("clientsounds_stream_s2c")
 
 hook.Add("PlayerSay","Clientsounds Text Sound",clientsounds_textsound)
+hook.Add("Initialize","Clientsounds Random",clientsounds_initialize)
