307d32eda7ad4bfd77aa76ca4ec52be4b06cb3ce
LanceXXXX <mastergarox@hotmail.com>
2016-11-01 23:10:06 -0400

    Update ca_server.lua

--- cook_anything/lua/autorun/Server/ca_server.lua
+++ cook_anything/lua/autorun/Server/ca_server.lua
@@ -1,2 +1,3 @@
-resource.AddFile("addons/cook_anything/materials/vgui/entities/ca_stove.vmt")
-resource.AddSingleFile("addons/cook_anything/sound/sounds/ca_eat.ogg")
+resource.AddSingleFile("materials/vgui/entities/ca_stove.vmt")
+resource.AddSingleFile("materials/vgui/entities/ca_stove.vtf")
+resource.AddSingleFile("sound/cook_anything/ca_eat.ogg")
